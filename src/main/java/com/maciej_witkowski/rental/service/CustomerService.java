3ef148aafc15e97d07a731fa8adaf495434c29ea
Maciej Witkowski <maciej.jan.witkowski@gmail.com>
2021-08-22 20:47:40 +0200

    Customer: baser service for business logic

---
+++ src/main/java/com/maciej_witkowski/rental/service/CustomerService.java
@@ -0,0 +1,17 @@
+package com.maciej_witkowski.rental.service;
+
+import com.maciej_witkowski.rental.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+}
