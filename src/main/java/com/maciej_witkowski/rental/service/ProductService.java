8b64c8ac7fc043314a3a5d495e67cc8a4d7ab746
Maciej Witkowski <maciej.jan.witkowski@gmail.com>
2021-08-22 21:07:10 +0200

    Product: base service for business logic

---
+++ src/main/java/com/maciej_witkowski/rental/service/ProductService.java
@@ -0,0 +1,17 @@
+package com.maciej_witkowski.rental.service;
+
+import com.maciej_witkowski.rental.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+}
