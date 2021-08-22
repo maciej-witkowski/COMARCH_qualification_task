1f68678a8062abe839fdc6269c9f64e6b0171889
Maciej Witkowski <maciej.jan.witkowski@gmail.com>
2021-08-22 20:59:51 +0200

    Customer: configuration adding test data to the DB

---
+++ src/main/java/com/maciej_witkowski/rental/config/CustomerConfig.java
@@ -0,0 +1,35 @@
+package com.maciej_witkowski.rental.config;
+
+import com.maciej_witkowski.rental.model.Customer;
+import com.maciej_witkowski.rental.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class CustomerConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunnerConsumer(CustomerRepository repository) {
+        return args -> {
+            Customer customer0 = new Customer(
+                    "Maciej",
+                    "Witkowski",
+                    "maciej.jan.witkowski@gmail.com",
+                    "723-377-677"
+            );
+
+            Customer customer1 = new Customer(
+                    "Kajetan",
+                    "Kęsik",
+                    "kesik@gmail.com",
+                    "689-634-852"
+            );
+
+            repository.saveAll(List.of(customer0, customer1));
+        };
+    }
+
+}
