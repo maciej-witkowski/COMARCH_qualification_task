4b15b571f8cc57498f982cc4cae752d72d95fb0a
Maciej Witkowski <maciej.jan.witkowski@gmail.com>
2021-08-23 05:36:27 +0200

    Orders: GET request with business logic

--- src/main/java/com/maciej_witkowski/rental/controller/OrdersController.java
+++ src/main/java/com/maciej_witkowski/rental/controller/OrdersController.java
@@ -1,8 +1,11 @@
 package com.maciej_witkowski.rental.controller;
 
+import com.maciej_witkowski.rental.model.Orders;
 import com.maciej_witkowski.rental.service.OrdersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/orders")
@@ -13,6 +16,11 @@ public class OrdersController {
     @Autowired
     public OrdersController(OrdersService orderService) {
         this.orderService = orderService;
+    }
+
+    @GetMapping
+    public List<Orders> getOrders() {
+        return orderService.getOrders();
     }
 
 }
--- src/main/java/com/maciej_witkowski/rental/service/OrdersService.java
+++ src/main/java/com/maciej_witkowski/rental/service/OrdersService.java
@@ -1,10 +1,13 @@
 package com.maciej_witkowski.rental.service;
 
+import com.maciej_witkowski.rental.model.Orders;
 import com.maciej_witkowski.rental.repository.CustomerRepository;
 import com.maciej_witkowski.rental.repository.OrdersRepository;
 import com.maciej_witkowski.rental.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrdersService {
@@ -18,6 +21,10 @@ public class OrdersService {
         this.ordersRepository = ordersRepository;
         this.productRepository = productRepository;
         this.customerRepository = customerRepository;
+    }
+
+    public List<Orders> getOrders() {
+        return ordersRepository.findAll();
     }
 
 }
