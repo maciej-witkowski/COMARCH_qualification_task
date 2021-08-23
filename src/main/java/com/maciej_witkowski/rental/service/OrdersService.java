43c6979ad3784664b61641180dce006dc4eb0584
Maciej Witkowski <maciej.jan.witkowski@gmail.com>
2021-08-23 05:34:09 +0200

    Orders: base service for business logic

---
+++ src/main/java/com/maciej_witkowski/rental/service/OrdersService.java
@@ -0,0 +1,23 @@
+package com.maciej_witkowski.rental.service;
+
+import com.maciej_witkowski.rental.repository.CustomerRepository;
+import com.maciej_witkowski.rental.repository.OrdersRepository;
+import com.maciej_witkowski.rental.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrdersService {
+
+    private final OrdersRepository ordersRepository;
+    private final ProductRepository productRepository;
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public OrdersService(OrdersRepository ordersRepository, ProductRepository productRepository, CustomerRepository customerRepository) {
+        this.ordersRepository = ordersRepository;
+        this.productRepository = productRepository;
+        this.customerRepository = customerRepository;
+    }
+
+}
